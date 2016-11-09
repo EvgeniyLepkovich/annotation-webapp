6dfeb3b76153b2634d00039bcc9fa6f1656adde1
Fene4ka_ <evgeniy.lepkovich@gmail.com>
2016-11-09 15:44:30 +0300

    First commit

---
+++ src/main/java/spring/test/annotation_webapp/config/AppConfig.java
@@ -0,0 +1,41 @@
+package spring.test.annotation_webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by Fene4ka_ on 09.11.2016.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("spring.test.annotation_webapp")
+//@Import({SecurityConfig.class})
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/jsp/**").addResourceLocations("/jsp/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Fene4ka_
+  Date: 09.11.2016
+  Time: 5:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>admin</h1>
+</body>
+</html>
